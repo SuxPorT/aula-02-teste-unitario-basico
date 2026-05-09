ec8203886e56f1540879b9498fa34dd2ceb6def0
Alexys Santiago <suxport@hotmail.com>
2026-05-09 10:03:59 -0300

    feat: add pipeline and test to fail

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -25,4 +25,8 @@ describe('Testes das 4 Operações Matemáticas', () => {
   test('Divisão: deve retornar mensagem de erro ao dividir por zero', () => {
     expect(dividir(10, 0)).toBe('Erro: Divisão por zero');
   });
+
+  test('Soma errada: deve retornar falha de teste para 10 + 5', () => {
+    expect(somar(10, 5)).toBe(25);
+  });
 });
